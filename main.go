d64517212dd6cc8d61a02343a1d9adbbadd73718
Gabriel Vasile <gabriel.vasile@email.com>
2025-08-11 12:50:05 +0900

    Update main.go

--- main.go
+++ main.go
@@ -32,7 +32,10 @@ const (
 	statusGood = "good"
 )
 
-var debug = os.Getenv("DEBUG") != ""
+var (
+	debug = os.Getenv("DEBUG") != ""
+	ci    = os.GetEnv("CI") != ""
+)
 
 func main() {
 	fmt.Println(`
@@ -133,7 +136,15 @@ func skipFile(mag, m string) bool {
 
 // mag calls the file magic detection.
 func mag(f string) string {
-	out, err := exec.Command("file", "-b", "--mime", f).Output()
+	out := []byte{}
+	var err error
+	if ci {
+		cmd := exec.Command("file", "-b", "-mime", "-m", "/usr/local/share/misc/magic.mgc", f)
+		cmd.Env = []string{"LD_PRELOAD=/usr/local/lib/libmagic.so.1.0.0"}
+		out, err = cmd.Output()
+	} else {
+		out, err = exec.Command("file", "-b", "--mime", f).Output()
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
