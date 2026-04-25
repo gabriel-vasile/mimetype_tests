04435be0e5aa9abc038e6d111cee769a8efa1dc3
Gabriel Vasile <gabriel.vasile@email.com>
2026-04-25 17:33:39 +0900

    a

---
+++ mp3dec/go.mod
@@ -0,0 +1,8 @@
+module asd
+
+go 1.25.0
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/tosone/minimp3 v1.0.2 // indirect
+)
---
+++ mp3dec/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	fs := allFilesInDir("../testfiles/minimp3/vectors")
+	for _, f := range fs {
+		decFile(f)
+	}
+}
+
+func decFile(f string) {
+	cmd := exec.Command("mp3info", "-p", "%L %S %u %b\n", f)
+	// if err := cmd.Run(); err != nil {
+	// 	// log.Fatal(err)
+	// }
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return
+	}
+	fmt.Println(f, string(out))
+}
+
+func allFilesInDir(dir string) []string {
+	ret := []string{}
+	err := filepath.Walk(dir,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if strings.Contains(path, "/.git/") {
+				return nil
+			}
+			if info.Size() == 0 {
+				return nil
+			}
+			if info.Mode().IsRegular() {
+				ret = append(ret, path)
+			}
+			return nil
+		})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return ret
+}
